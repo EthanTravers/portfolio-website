5a96e26b7987a4288c96fa360775f07e98898e22
EthanTravers <et2g21@soton.ac.uk>
2024-07-04 18:58:24 +0100

    Added React Header component and working on different views for pc, tablet and mobile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import { useMediaQuery } from 'react-responsive';
 
 function App() {
+  const name = 'Ethan';
+  const x = false;
+  const isDesktop = useMediaQuery({ minWidth: 1224 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1224 });
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <Header />
+      <h1>Hello from React</h1>
+      <h2>Hello {name}</h2>
+      <h2>Hello {x ? 'Yes' : 'No'}</h2>
     </div>
   );
 }
